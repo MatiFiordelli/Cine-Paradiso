a1bcf0557161915a74aca98082f37b7f3d5c38f3
Matias Fiordelli <>
2024-06-11 20:05:33 -0300

    popup successful payment

--- src/components/HomeComponents/index.jsx
+++ src/components/HomeComponents/index.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FadeEffect from "../Resources/FadeEffect";
 import TopPresentation from "./TopPresentation";
 import MovieBillboard from "./MovieBillboard";
 import UpcomingMovies from "./UpcomingMovies";
+import Swal from "sweetalert2";
 
 export default function HomeComponent() {
+  useEffect(() => {
+    // Verificar si estamos en la página de éxito de pago y mostrar el SweetAlert2
+    if (window.location.pathname === "/payment_success") {
+      Swal.fire({
+        icon: "success",
+        title: "Pago exitoso",
+        text: "¡Gracias por su compra!",
+        confirmButtonText: "Ok",
+      }).then(() => {
+        window.location.href = "https://cine-paradiso.vercel.app"; // Redireccionar después de hacer clic en OK
+      });
+    }
+  }, []);
+
+
   return (
     <FadeEffect>
       <main style={{ minHeight: "80vh" }}>
